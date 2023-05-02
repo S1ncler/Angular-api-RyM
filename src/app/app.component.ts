85718921b2873b6b45f8858093c6b1d598af8423
S1ncler <daniel_felipe_c11@hotmail.com>
2023-05-02 11:58:37 -0500

    hola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,23 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-<<<<<<< HEAD
-export class AppComponent implements OnInit{
-  title = 'angular-rick-y-morty';
-  constructor() {}
-
-  private url = "https://rickandmortyapi.com/api/character";
-  personajes:any;
-
-  ngOnInit(): void {
-    fetch(this.url)
-    .then(response => response.json())
-    .then((data) => {
-      this.personajes = data.results
-    })    
-  }
-  
-=======
 export class AppComponent implements OnInit {
   title = 'angular-rick-y-morty';
   constructor() {}
@@ -36,5 +19,4 @@ export class AppComponent implements OnInit {
         this.personajes = data.results;
       });
   }
->>>>>>> 79e2c95 (consumo de api con angular)
 }
