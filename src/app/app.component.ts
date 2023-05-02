21b4d3c3cd00ff797dd9fb042be63f0dbdad6bf0
S1ncler <daniel_felipe_c11@hotmail.com>
2023-05-02 11:40:25 -0500

    consumo de api con angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+<<<<<<< HEAD
 export class AppComponent implements OnInit{
   title = 'angular-rick-y-morty';
   constructor() {}
@@ -20,4 +21,20 @@ export class AppComponent implements OnInit{
     })    
   }
   
+=======
+export class AppComponent implements OnInit {
+  title = 'angular-rick-y-morty';
+  constructor() {}
+
+  private url = 'https://rickandmortyapi.com/api/character';
+  personajes: any;
+
+  ngOnInit(): void {
+    fetch(this.url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.personajes = data.results;
+      });
+  }
+>>>>>>> 79e2c95 (consumo de api con angular)
 }
